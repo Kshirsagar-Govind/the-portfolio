6ea646e24622afdab80cdec94ed15075f59887f9
Kshirsagar-Govind <88773342+Kshirsagar-Govind@users.noreply.github.com>
2022-04-09 17:48:24 +0530

    new changes

---
+++ src/Components/WorkPages/hackx-node-page.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "../CSS/main-style.css";
+import "../CSS/comps-styles.css";
+import "../CSS/my-work-style.css";
+
+function HackxPage() {
+  return (
+    <div className="">
+      <h1>HACK-X NODE</h1>
+    </div>
+  );
+}
+
+export default HackxPage;
---
+++ src/Components/WorkPages/note-it-down-page.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "../CSS/main-style.css";
+import "../CSS/comps-styles.css";
+import "../CSS/my-work-style.css";
+
+function NoteItDownPage() {
+  return (
+    <div className="">
+      <h1>Note It Down</h1>
+    </div>
+  );
+}
+
+export default NoteItDownPage;
