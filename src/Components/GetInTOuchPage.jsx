2306ee5f36850bb5e46bd9f0af8f1993eb0dcc44
Kshirsagar-Govind <88773342+Kshirsagar-Govind@users.noreply.github.com>
2022-04-12 22:57:18 +0530

    new changes

--- src/Components/GetInTOuchPage.jsx
+++ src/Components/GetInTOuchPage.jsx
@@ -1,7 +1,7 @@
 import React, { Component, useState } from "react";
 import "./CSS/main-style.css";
 import "./CSS/comps-styles.css";
-import "./CSS/my-self-style.css";
+import "./CSS/get-in-touch-style.css";
 
 function GetInTouch() {
   return (
@@ -13,6 +13,28 @@ function GetInTouch() {
           <span className="prim-color"> Talk</span>
         </h1>
       </div>
+      <form>
+        <div className="form">
+          <div className="input-div">
+            <label>Your Name</label>
+            <input type="text" placeholder="enter your name" />
+          </div>
+          <div className="input-div">
+            <label>Your Email</label>
+            <input type="email" placeholder="enter your email" />
+          </div>
+          <div className="input-div">
+            <label>Your Subject</label>
+            <input type="text" placeholder="enter subject" />
+          </div>
+          <div>
+            <textarea placeholder="enter message" />
+          </div>
+          <div className="make-it-centered">
+            <input className="work-btn" type="submit" value="submit" />
+          </div>
+        </div>
+      </form>
     </div>
   );
 }
